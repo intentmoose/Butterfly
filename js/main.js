8b77b3eb9fa2741d33cbc1f85a381ffb5941d664
intentmoose <cariel226@gmail.com>
2020-11-17 08:53:12 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.querySelector('a-assets').addEventListener('loaded', function (evt) {
+    document.querySelector('.loading-overlay').style.display = 'none';
+    document.querySelector('.btns-wings').style.display = 'flex';
+
+});
+function changeWings(param) {
+    let wings = document.querySelector('.wings');
+    wings.setAttribute('src', `/assets/model/wings${param}.glb`)
+}
