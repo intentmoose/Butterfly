a43f4a1e4736cd2d0c2823e795c569a0ad36fb87
intentmoose <cariel226@gmail.com>
2020-11-17 19:32:45 +0200

    wing changing changes by visiblity instead of source

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
+var currSelected = 0;
 document.querySelector('a-assets').addEventListener('loaded', function (evt) {
     document.querySelector('.loading-overlay').style.display = 'none';
     document.querySelector('.btns-wings').style.display = 'flex';
 
 });
 function changeWings(param) {
-    let wings = document.querySelector('.wings');
-    wings.setAttribute('src', `../assets/model/wings${param}.glb`)
+    let prevWings = document.querySelector('.wings' + currSelected);
+    let wingsToChange = document.querySelector('.wings' + param);
+
+    prevWings.setAttribute('visible', `false`)
+    currSelected = param;
+    wingsToChange.setAttribute('visible', `true`)
 }
